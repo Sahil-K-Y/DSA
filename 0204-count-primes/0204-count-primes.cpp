68d2f33216bcc684234a4ba394125fef9c3c41d4
SAHIL KUMAR <Kumsahil7973@gmail.com>
2026-09-14 00:38:36 +0530

    Time: 634 ms (18.83%), Space: 40.3 MB (33.03%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -4,13 +4,12 @@ public:
         if (n <= 2) return 0;
 
         vector<bool> isPrime(n, true);
-        // Pehle se count: 2 ko include karke baaki sirf odd numbers ka count
+
         int count = n / 2;
 
-        for (long long i = 3; i * i < n; i += 2) {
+        for (int i = 3; 1LL * i * i < n; i += 2) {
             if (isPrime[i]) {
-                // i odd hai, isliye i*i + odd step (2*i) se hamesha odd multiples hi milenge
-                for (long long j = i * i; j < n; j += 2 * i) {
+                for (int j = i * i; j < n; j += 2 * i) {
                     if (isPrime[j]) {
                         isPrime[j] = false;
                         count--;
