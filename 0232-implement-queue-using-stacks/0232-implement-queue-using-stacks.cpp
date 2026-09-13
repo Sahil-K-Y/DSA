5fc8c0e7ef58bbe0da0251a6246d53af976d32f2
SAHIL KUMAR <Kumsahil7973@gmail.com>
2026-09-13 22:48:20 +0530

    Time: 0 ms (100%), Space: 10.1 MB (87.36%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -0,0 +1,47 @@
+class MyQueue {
+private:
+    stack<int>in;
+    stack<int>out;
+    void shift(){
+        if(out.empty()){
+            while(!in.empty()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+    }
+
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        in.push(x);
+    }
+    
+    int pop() {
+        shift();
+        int val=out.top();
+        out.pop();
+        return val;
+    }
+    
+    int peek() {
+        shift();
+        return out.top();
+    }
+    
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
